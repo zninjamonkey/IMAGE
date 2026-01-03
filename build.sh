03460fe3d91cf8390d5b2bcbe0e49806de83d0d6
Anson Kiehl <ansonkiehl@gmail.com>
2026-01-02 20:25:16 -0700

    Experimenting with uinput for input emualation

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-gcc -o gamepadtest gamepad-polling.c -I./external/SDL/include/SDL3 -L./external/SDL/build -lSDL3 -Wl,-rpath,'$ORIGIN/lib'
-mv gamepadtest ./build
+gcc -o joysticktest joystick-polling.c -I./external/SDL/include/SDL3 -L./external/SDL/build -lSDL3 -Wl,-rpath,'$ORIGIN/lib'
+mv joysticktest ./build
