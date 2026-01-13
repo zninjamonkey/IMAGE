ba7f77e58c88a1cc518f2de9b5ee675c8ccb40c3
Anson Kiehl <ansonkiehl@gmail.com>
2026-01-12 20:52:15 -0700

    realized I don't need a js-read.h file

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
-gcc -c -o ./build/joysticktest.o ./source/joystick-events.c 
+# gcc -c -o ./build/joysticktest.o ./source/joystick-events.c 
+gcc -c -o ./build/readjs.o ./source/js-read.c
 
 gcc -c -o ./build/virtpad.o ./source/virtual-gamepad.c
 
-gcc -o ./build/image ./build/virtpad.o ./build/joysticktest.o \
+gcc -o ./build/image ./build/virtpad.o ./build/readjs.o \
  -I./external/SDL/include/SDL3 -L./external/SDL/build \
  -lSDL3 \
  -Wl,-rpath,'$ORIGIN/lib' \
---
+++ source/js-read.h
@@ -0,0 +1,6 @@
+#ifndef JS_READ_H
+#define JS_READ_H
+
+struct GamepadStatus gpad_state;
+
+#endif
