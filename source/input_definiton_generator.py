cb2436a1d0bfda156aa410811deca99199c806c7
anson <idk@bro.com>
2026-08-15 16:40:47 -0600

    created basic input mapper and recreated joystick reader in python using evdev

--- source/input_definiton_generator.py
+++ source/input_definiton_generator.py
@@ -1,21 +1,105 @@
 import evdev
-
-path = "new-input-definitions.h"
-
-print("writing to " + path)
-
-file = open("./" + path, "+a")
-
-file.write("#ifndef INPUT_DEFINITIONS_H\n#define INPUT_DEFINITIONS_H\n")
-
-print("press the A button")
+import time
 
 device = evdev.InputDevice('/dev/input/event6')
 print(device)
-# device /dev/input/event1, name "USB Keyboard", phys "usb-0000:00:12.1-2/input0"
 
-for event in device.read_loop():
-    if event.type == evdev.ecodes.EV_KEY:
-        print(evdev.categorize(event))
+path = device.phys + "-input-definitions.csv"
+
+print("writing to " + path)
+
+file = open("./" + path, "w")
+
+used_codes = []
+
+def get_next_key(name):
+    waiting = True
+    while waiting:
+        time.sleep(0.1)
+        for event in device.read_loop():
+            if event.type == evdev.ecodes.EV_KEY and event.value == 1 and event.code not in used_codes:
+                print("boo ya")
+                file.write(name + "," + str(event.code) + "\n")
+                used_codes.append(event.code)
+                waiting = False
+                break
+
+def get_next_axis(name, bound):
+    waiting = True
+    while waiting:
+        time.sleep(1)
+        for event in device.read_loop():
+            if (event.code in used_codes):
+                continue
+            if abs(event.value - 32767) < 5000:
+                continue
+            if event.type == evdev.ecodes.EV_ABS and abs(event.value) > bound :
+                print("boo ya")
+                file.write(name + "," + str(event.code) + "\n")
+                used_codes.append(event.code)
+                waiting = False
+                break
 
 
+# get_next_axis("dfs", 64000)
+# get_next_axis("bru", 1000)
+# get_next_axis("dude", 0.5)
+
+
+
+print("press the A button")
+get_next_key("A_BTN")
+
+print("press the B button")
+get_next_key("B_BTN")
+
+print("press the X button")
+get_next_key("X_BTN")
+
+print("press the Y button")
+get_next_key("Y_BTN")
+
+print("press the right bumper")
+get_next_key("R_BUMP")
+
+print("press the left bumper")
+get_next_key("L_BUMP")
+
+print("press the right stick")
+get_next_key("R_STICK")
+
+print("press the left stick")
+get_next_key("L_STICK")
+
+print("press the start button")
+get_next_key("START_BTN")
+
+print("press the select button")
+get_next_key("SELECT_BTN")
+
+print("press the home button")
+get_next_key("HOME_BTN")
+
+print("press the right trigger")
+get_next_axis("R_TRIG", 1000)
+
+print("press the left trigger")
+get_next_axis("L_TRIG", 1000)
+
+print("push the right joystick right")
+get_next_axis("R_X_STICK", 64000)
+
+print("push the right joystick down")
+get_next_axis("R_Y_STICK", 64000)
+
+print("push the left joystick right")
+get_next_axis("L_X_STICK", 64000)
+
+print("push the left joystick down")
+get_next_axis("L_Y_STICK", 64000)
+
+print("press the right dpad arrow")
+get_next_axis("X_PAD", 0.5)
+
+print("press the down dpad arrow")
+get_next_axis("Y_PAD", 0.5)
---
+++ source/js-read.py
@@ -0,0 +1,57 @@
+import evdev
+import csv
+
+
+input_definitions_csv = open('./input-definitions.csv', 'r')
+reader = csv.reader(input_definitions_csv)
+input_definitions = list(reader)
+print(input_definitions)
+
+device = evdev.InputDevice('/dev/input/event6')
+print(device)
+
+# device /dev/input/event1, name "USB Keyboard", phys "usb-0000:00:12.1-2/input0"
+
+def handle_input(event):
+    global input_definitions
+
+    if event.type == evdev.ecodes.EV_KEY or event.type == evdev.ecodes.EV_ABS:
+        # print(event.code)
+        # print("done")
+
+        # if event.code == 1:
+        #     print(event.value)
+
+        if event.code == int(input_definitions[0][1]):
+            print("A button pressed")
+        elif event.code == int(input_definitions[1][1]):
+            print("B button pressed")
+        elif event.code == int(input_definitions[2][1]):
+            print("X button pressed")
+        elif event.code == int(input_definitions[3][1]):
+            print("Y button pressed")
+        elif event.code == int(input_definitions[4][1]):
+            print("right bumper pressed")
+        elif event.code == int(input_definitions[5][1]):
+            print("left bumper pressed")
+        # elif event.code == int(input_definitions[3][1]):
+        #     print("Y button pressed")
+        # elif event.code == int(input_definitions[3][1]):
+        #     print("Y button pressed")
+        # elif event.code == int(input_definitions[3][1]):
+        #     print("Y button pressed")
+        # elif event.code == int(input_definitions[3][1]):
+        #     print("Y button pressed")
+        # elif event.code == int(input_definitions[3][1]):
+        #     print("Y button pressed")
+        # elif event.code == int(input_definitions[3][1]):
+        #     print("Y button pressed")
+        
+
+for event in device.read_loop():
+
+    handle_input(event)
+    # if event.type == evdev.ecodes.EV_KEY:
+    #     print(evdev.categorize(event))
+    # if event.type == evdev.ecodes.EV_ABS:
+    #     print()
